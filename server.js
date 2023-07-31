b055f384dd73a3f355086dc5215da97dcc0a9f6c
Cristofferb7 <136852733+Cristofferb7@users.noreply.github.com>
2023-07-31 17:04:01 -0400

    added new data to db

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require("express"); // load the installed package
 const app = express(); // use the package to create a server called "app"
 
 const path = require("path")
+const fs = require("fs")
 
 const PORT = 3001;
 
 // middlewares
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: false}))
+app.use(express.json());
 
-// create routes
+// create HTML routes
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
@@ -17,14 +20,33 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 })
 
-// app.get("/assets/css/styles.css", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./public/assets/css/styles.css"))
-// })
+// create API routes
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        const db = JSON.parse(data)
 
-// app.get("/assets/js/index.js", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./public/assets/js/index.js"))
-// })
+        res.json(db)
+    })
+})
 
+app.post("/api/notes", (req, res) => {
+    // check the data sent
+    console.log(req.body)
+
+    // read the current db
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        const db = JSON.parse(data)
+
+        // add new data to db
+        db.push(req.body);
+
+        // update the file for the db
+        fs.writeFile("./db/db.json", JSON.stringify(db), () => {
+            res.json(db)
+        })
+        
+    })
+})
 
 app.listen(PORT, () => {
     // console.log("Server is running on PORT " + PORT)
